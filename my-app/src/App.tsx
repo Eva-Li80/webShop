b1020e7a97b9a4dc118db20d60507501aa744513
Eva-Li80 <evaliboden@gmail.com>
2024-01-16 16:31:09 +0100

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import "./styles/main.scss"
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <h1>test</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
